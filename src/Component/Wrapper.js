82b63bafc1ef2cdaaf0f902c291cda6e9505ebcc
hibaiqbalbegum <hib4@njit.edu>
2023-04-06 09:28:56 -0400

    Added my project

---
+++ src/Component/Wrapper.js
@@ -0,0 +1,10 @@
+import react from "react";
+
+const Wrapper=({children})=>{
+    
+        return(
+            <div className="wrapper">{children}</div>
+        );
+    
+}
+export default Wrapper;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './Component/App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
+const container = document.getElementById("root");
+
+const root = createRoot(container);
+
+class Application extends React.Component{
+  render(){
+    return(
+      <div>
+        <App/>
+      </div>
+    );
+  }
+}
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+<React.StrictMode>
+  
+  <Application/>
+  
+</React.StrictMode>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  );
+
